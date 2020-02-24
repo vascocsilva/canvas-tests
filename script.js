19d4278a2db496a0a9a9645268a01b77569c42a3
Vasco Silva <vasco.silva@tekzenit.com>
2020-02-24 17:20:14 +0000

    Add particles interaction

--- script.js
+++ script.js
@@ -4,18 +4,47 @@ canvas.height = window.innerHeight
 
 const c = canvas.getContext('2d')
 
-function Circle(x, y, dx, dy, radius, alpha = 1) {
+const mouse = {
+  x: null,
+  y: null,
+}
+
+const colorArray = [
+  '#d6fff6',
+  '#231651',
+  '#4dccbd',
+  '#2374ab',
+  '#ff1100',
+]
+
+window.addEventListener('mousemove', (e) => {
+  mouse.x = e.x
+  mouse.y = e.y
+})
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+  init()
+})
+
+const maxRadius = 60
+const minRadius = 10
+
+function Circle(x, y, dx, dy, radius) {
   this.x = x
   this.y = y
   this.dx = dx
   this.dy = dy
   this.radius = radius
-  this.alpha = alpha
+  this.originalRadius = radius
+  this.fillColor = colorArray[Math.floor(Math.random() * colorArray.length)]
 
   this.draw = () => {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = `rgba(255, 255, 255, ${this.alpha})`
+    c.fillStyle = this.fillColor
     c.fill()
   }
 
@@ -30,27 +59,36 @@ function Circle(x, y, dx, dy, radius, alpha = 1) {
 
     this.x += this.dx
     this.y += this.dy
-    this.alpha = Math.random()
+
+    if (
+      mouse.x - this.x < 50
+      && mouse.x - this.x > -50
+      && mouse.y - this.y < 50
+      && mouse.y - this.y > -50
+      && this.radius < maxRadius
+    ) {
+      this.radius += 1
+    } else if (this.radius > this.originalRadius) {
+      this.radius -= 1
+    }
 
     this.draw()
   }
 }
 
-let circle = new Circle(200, 200, 4, 4, 30, 0.6)
+let circleArray = []
 
+function init() {
+  circleArray = []
+  for (let i = 0; i < 500; i++) {
+    const radius = Math.random() * minRadius + 1
+    const x = Math.random() * (innerWidth - radius * 2) + radius
+    const y = Math.random() * (innerHeight - radius * 2) + radius
+    const dx = (Math.random() - 0.5) * 4
+    const dy = (Math.random() - 0.5) * 4
 
-const radius = 30
-
-const circleArray = []
-for (let i = 0; i < 200; i++) {
-  const radius = Math.random() * 3
-  const x = Math.random() * (innerWidth - radius * 2) + radius
-  const y = Math.random() * (innerHeight - radius * 2) + radius
-  const dx = (Math.random() - 0.5) * 4
-  const dy = (Math.random() - 0.5) * 4
-  const alpha = Math.random()
-
-  circleArray.push(new Circle(x, y, dx, dy, radius, alpha))
+    circleArray.push(new Circle(x, y, dx, dy, radius))
+  }
 }
 
 function animate() {
@@ -63,4 +101,5 @@ function animate() {
 
 }
 
+init()
 animate()
