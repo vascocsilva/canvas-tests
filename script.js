1e7c33e88ada43db256334ccba6e1668dda32fcd
Vasco Silva <vasco.s.rebolo@gmail.com>
2020-02-24 21:26:44 +0000

    Add stars motion

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ canvas.height = window.innerHeight
 
 const c = canvas.getContext('2d')
 
-function Circle(x, y, dx, dy, radius, alpha = 1) {
+function addCircle(key, randomY = true) {
+  const radius = Math.random() * 3
+  const x = Math.random() * (innerWidth - radius * 2) + radius
+  const y = randomY
+    ? Math.random() * (innerHeight - radius * 2) + radius
+    : 0
+  const dx = 0//(Math.random() - 0.5) * 4
+  const dy = 1//(Math.random() - 0.5) * 4
+  const alpha = Math.random()
+  circles.set(key, new Circle(x, y, dx, dy, radius, alpha, key))
+}
+
+function Circle(x, y, dx, dy, radius, alpha = 1, key) {
   this.x = x
   this.y = y
   this.dx = dx
   this.dy = dy
   this.radius = radius
   this.alpha = alpha
+  this.key = key
 
   this.draw = () => {
     c.beginPath()
@@ -20,12 +33,14 @@ function Circle(x, y, dx, dy, radius, alpha = 1) {
   }
 
   this.update = () => {
-    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-      this.dx = -this.dx
+    if (this.x > innerWidth || this.x + this.radius < 0) {
+      circles.delete(this.key)
+      addCircle(_.uniqueId(), false)
     }
 
-    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-      this.dy = -this.dy
+    if (this.y > innerHeight || this.y + this.radius < 0) {
+      circles.delete(this.key)
+      addCircle(_.uniqueId(), false)
     }
 
     this.x += this.dx
@@ -41,26 +56,21 @@ let circle = new Circle(200, 200, 4, 4, 30, 0.6)
 
 const radius = 30
 
-const circleArray = []
-for (let i = 0; i < 200; i++) {
-  const radius = Math.random() * 3
-  const x = Math.random() * (innerWidth - radius * 2) + radius
-  const y = Math.random() * (innerHeight - radius * 2) + radius
-  const dx = (Math.random() - 0.5) * 4
-  const dy = (Math.random() - 0.5) * 4
-  const alpha = Math.random()
-
-  circleArray.push(new Circle(x, y, dx, dy, radius, alpha))
+const circles = new Map()
+for (let i = 0; i < 500; i++) {
+  addCircle(i)
 }
+
+console.log(circles);
+
 
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, innerWidth, innerHeight)
 
-  circleArray.forEach(circle => {
-    circle.update()
-  });
-
+  for (let [key, value] of circles) {
+    value.update()
+  }
 }
 
 animate()
